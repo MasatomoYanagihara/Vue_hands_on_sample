410d7b9e86f9e3deb5917affd412426a1f863fae
Yana <masatomo.yanagihara.jp@gmail.com>
2020-10-29 14:54:14 +0900

    08.4 空の画面コンポーネントを作る

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
     <Router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import { defineComponent } from '@vue/composition-api';
+import FooterComponent from '@/components/Footer.vue';
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    Footer: FooterComponent,
+  },
+});
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <Footer />
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
