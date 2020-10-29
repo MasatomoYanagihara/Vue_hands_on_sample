31a1926f5c5f9a2c37425c3950bbe0438af75f90
Yana <masatomo.yanagihara.jp@gmail.com>
2020-10-29 15:32:57 +0900

    09.3 Vue Routerの設定と使用

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  singleQuote: true,
+  singleQuote: true, // シングルクォートに統一
   trailingComma: 'all',
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
     <h1>ホーム</h1>
+    <p><Router-link to="sign-in">サインイン</Router-link></p>
+    <p><button @click="calendar">カレンダー</button></p>
+    <p><button @click="profile">プロフィール</button></p>
+    <p><button @click="share">共有</button></p>
   </div>
 </template>
 
 <script>
 import { defineComponent } from '@vue/composition-api';
 
-export default defineComponent({});
+export default defineComponent({
+  setup(prop, context) {
+    // context.root.$routerでVue Routerインスタンスにアクセス
+    // push( path, 遷移成功時に呼び出されるコールバック関数（省略可）, 遷移中断時に呼び出されるコールバック関数（省略可） )
+    const calendar = () => {
+      context.root.$router.push(
+        'calendar/month',
+        // nameで指定 { name: 'carendar', params: { type: 'month } }
+        () => {},
+        () => {},
+      );
+    };
+    const profile = () => {
+      context.root.$router.push(
+        'profile',
+        () => {},
+        () => {},
+      );
+    };
+    const share = () => {
+      context.root.$router.push(
+        'share',
+        () => {},
+        () => {},
+      );
+    };
+
+    return {
+      calendar,
+      profile,
+      share,
+    };
+  },
+});
 </script>
