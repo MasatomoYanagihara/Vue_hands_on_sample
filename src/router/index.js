a1710a611788cea93b55b6ac918be6b34a6c4019
Yana <masatomo.yanagihara.jp@gmail.com>
2020-11-02 19:08:01 +0900

    17.3 認可

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ProfileComponent from '@/views/Profile.vue';
 import ShareComponent from '@/views/Share.vue';
 import SignInComponent from '@/views/SignIn.vue';
 import NotFoundComponent from '@/views/NotFound.vue';
+import store from '@/store/index';
 
 Vue.use(VueRouter);
 
@@ -76,6 +77,28 @@ router.afterEach(to => {
 
   // meta.titleがあればtitleに表示
   document.title = to.meta.title;
+});
+
+// beforeEachナビゲーションガード
+// ナビゲーションを行う前に処理を挟む
+// to: ナビゲーション先のルートオブジェクト
+// from: ナビゲーション元のルートオブジェクト
+// next: ナビゲーションを行う関数
+router.beforeEach((to, from, next) => {
+  // 次のパスが'/sign-in'なら遷移させる
+  if (to.path === '/sign-in') {
+    next();
+    return;
+  }
+
+  // profile情報を保持していたら遷移させる
+  if (store.getters['profile/profile']) {
+    next();
+    return;
+  }
+
+  // それ以外はサインイン画面に遷移させる
+  next('/sign-in');
 });
 
 export default router;
